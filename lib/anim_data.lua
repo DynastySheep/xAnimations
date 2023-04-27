b1e6ed44c9ae467d48cf52385be8e2b403687172
DynastySheep <dynastyrd@gmail.com>
2023-04-27 19:03:53 +0100

    from json to lua

---
+++ lib/anim_data.lua
@@ -0,0 +1,11 @@
+return {
+    sleep = {
+        scenario = "WORLD_HUMAN_BUM_SLUMPED",
+        controllable = false,
+    },
+    spy = {
+        scenario = "WORLD_HUMAN_BINOCULARS",
+        controllable = true,
+    },
+    -- Add more animations here
+}
